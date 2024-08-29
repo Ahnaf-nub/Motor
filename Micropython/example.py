027bc0ed939c80d68b0ec7e4056026cfcdc2e6f0
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-08-29 11:58:56 +0600

    Create example.py

---
+++ Micropython/example.py
@@ -0,0 +1,16 @@
+from motor import Motor
+
+# Define motor pins
+motor_pins = [2, 6, 7, 4, 5, 3]
+# Change state to change the rotattion of motor
+motorAdir = False
+motorBdir = True
+# Initialize MotorDriver instance
+motor_driver = MotorDriver(motor_pins)
+
+# Example usage in the main loop
+while True:
+    motor_driver.motor(512, 512, motorAdir, motorBdir)  # Forward direction for both motors
+    time.sleep(1)
+    motor_driver.motor(512, 512, motorAdir, motorAdir)  # Reverse direction for both motors
+    time.sleep(1)
